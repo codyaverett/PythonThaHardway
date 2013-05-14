e485d7f42ee2e978f49ae0690f065d90a50cf84d
Cody Averett <codyaverett@gmail.com>
2013-05-13 23:50:08 -0500

    Day 2
    More printing :P

---
+++ ex5.py
@@ -0,0 +1,17 @@
+# Variables and printing :D
+
+my_name = 'Cody Averett'
+my_age = '21'
+my_height = 74  # inches
+my_weight = 130 # lbs
+my_eyes = 'red'
+my_teeth = 'white'
+
+print "lets talk about me, my name is %s" % my_name
+print "I am %s years old" % my_age
+print "I just so happen to be %d inches tall" % my_height
+print "I weigh about %d pounds" %  my_weight
+
+print "This will print no matter what %r " % my_teeth
+
+#That works
