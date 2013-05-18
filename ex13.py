2c79ff9c6b7db072960a5f461ffa196f845c269a
Cody Averett <codyaverett@gmail.com>
2013-05-17 22:15:00 -0500

    Added example 13

---
+++ ex13.py
@@ -0,0 +1,10 @@
+# exercise 13 : Parameters and aurguments
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called: %r" % script
+print "Your first variable is: %r" % first
+print "Your second variable is: %r" % second
+print "Your third variable is: %r" % third
